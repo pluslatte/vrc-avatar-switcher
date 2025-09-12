374aa8d0106379b43c91e6f8ad179bd24a131180
pluslatte <debrisfield0201@gmail.com>
2025-09-12 22:23:19 +0900

    pnpm tauri add store

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -17,6 +17,7 @@ async fn command_fetch_avatars(
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![command_fetch_avatars])
         .run(tauri::generate_context!())

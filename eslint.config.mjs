9b3d7371cc94f5f7940ebe169e1a5495665d1332
pluslatte <debrisfield0201@gmail.com>
2025-09-12 19:34:26 +0900

    準備

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,8 @@ import eslint from '@eslint/js';
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
-export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-);
+export default defineConfig({
+  ...eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ignores: ['postcss.config.cjs']
+});

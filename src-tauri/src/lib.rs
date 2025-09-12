8e00a30adad5ba1aa6fb91c045b76c1dfa292860
pluslatte <debrisfield0201@gmail.com>
2025-09-13 00:17:35 +0900

    command new auth

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,9 +2,14 @@ mod auth;
 mod avatars;
 mod config;
 
+use serde::{Deserialize, Serialize};
 use vrchatapi::models::Avatar;
 
-use crate::{avatars::fetch_avatars, config::create_configuration_from_raw_cookies};
+use crate::{
+    auth::{get_new_auth_cookie_without_2fa, AuthCookieOk},
+    avatars::fetch_avatars,
+    config::create_configuration_from_raw_cookies,
+};
 
 #[tauri::command]
 async fn command_fetch_avatars(
@@ -15,17 +20,60 @@ async fn command_fetch_avatars(
     fetch_avatars(&config).await
 }
 
-// #[tauri::command]
-// async fn command_login(username: &str, password: &str) -> Result<, String> {
-
-// }
+#[derive(Debug, Serialize, Deserialize)]
+enum CommandLoginStatus {
+    Success = 0,
+    Requires2FA = 1,
+    RequiresEmail2FA = 2,
+}
+#[derive(Debug, Serialize, Deserialize)]
+struct CommandLoginOk {
+    status: CommandLoginStatus,
+    auth_cookie: String,
+    two_fa_cookie: Option<String>,
+}
+#[tauri::command]
+async fn command_new_auth(username: &str, password: &str) -> Result<String, String> {
+    match get_new_auth_cookie_without_2fa(username, password).await? {
+        AuthCookieOk::Success {
+            auth_cookie,
+            two_fa_cookie,
+        } => Ok(serde_json::to_string(&CommandLoginOk {
+            status: CommandLoginStatus::Success,
+            auth_cookie,
+            two_fa_cookie,
+        })
+        .map_err(|e| e.to_string())?),
+        AuthCookieOk::RequiresEmail2FA {
+            auth_cookie,
+            two_fa_cookie,
+        } => Ok(serde_json::to_string(&CommandLoginOk {
+            status: CommandLoginStatus::RequiresEmail2FA,
+            auth_cookie,
+            two_fa_cookie,
+        })
+        .map_err(|e| e.to_string())?),
+        AuthCookieOk::Requires2FA {
+            auth_cookie,
+            two_fa_cookie,
+        } => Ok(serde_json::to_string(&CommandLoginOk {
+            status: CommandLoginStatus::Requires2FA,
+            auth_cookie,
+            two_fa_cookie,
+        })
+        .map_err(|e| e.to_string())?),
+    }
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![command_fetch_avatars])
+        .invoke_handler(tauri::generate_handler![
+            command_fetch_avatars,
+            command_new_auth
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }

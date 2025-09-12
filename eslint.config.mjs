2e1508d9f18abc024c0bb05ca2c64ea6f96180b9
pluslatte <debrisfield0201@gmail.com>
2025-09-12 22:03:37 +0900

    eslint の設定

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,26 @@
 // @ts-check
 
 import eslint from '@eslint/js';
-import { defineConfig } from 'eslint/config';
+import { defineConfig } from 'eslint/config'
+import pluginQuery from '@tanstack/eslint-plugin-query';
 import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
 
-export default defineConfig({
-  ...eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  ignores: ['postcss.config.cjs']
-});
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  pluginQuery.configs['flat/recommended'],
+
+  [
+    {
+      ignores: ['postcss.config.cjs'],
+    },
+    {
+      plugins: {
+        react: pluginReact,
+        reactHooks: pluginReactHooks,
+      }
+    }
+  ]
+);

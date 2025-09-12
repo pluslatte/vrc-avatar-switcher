cbf38c92d978d60be83881babec5f7a201c28c7f
pluslatte <debrisfield0201@gmail.com>
2025-09-12 20:36:34 +0900

    お試し

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,88 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::{str::FromStr, sync::Arc, thread::sleep, time::Duration};
+
+use reqwest::cookie::CookieStore;
+use tauri::http::HeaderValue;
+use vrchatapi::apis::{self, configuration::Configuration};
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+struct Avatar {
+    id: String,
+    name: String,
+    image_url: String,
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn fetch_avatars(raw_auth_cookie: &str, raw_2fa_cookie: &str) -> Result<Vec<Avatar>, String> {
+    let mut out = Vec::new();
+    let mut avatar_count: usize = 0;
+
+    let jar = Arc::new(reqwest::cookie::Jar::default());
+    jar.set_cookies(
+        &mut [
+            HeaderValue::from_str(&format!("{raw_auth_cookie}; {raw_2fa_cookie}"))
+                .expect("Invalid cookie string"),
+        ]
+        .iter(),
+        &url::Url::from_str("https://api.vrchat.cloud").expect("Invalid URL"),
+    );
+    let config = Configuration {
+        user_agent: Some("avatar-switcher/0.1.0".to_string()),
+        client: reqwest::Client::builder()
+            .cookie_provider(jar)
+            .build()
+            .expect("Failed to build reqwest client"),
+        ..Default::default()
+    };
+
+    loop {
+        let avatars = apis::avatars_api::search_avatars(
+            &config,
+            Some(false),
+            Some(vrchatapi::models::SortOption::Name),
+            Some("me"),
+            None,
+            Some(60),
+            None,
+            Some(avatar_count.try_into().expect("Negative avatar count wtf")),
+            None,
+            None,
+            Some(vrchatapi::models::ReleaseStatus::All),
+            None,
+            None,
+            None,
+        )
+        .await;
+
+        match avatars {
+            Ok(avatars) => {
+                if avatars.is_empty() {
+                    return Ok(out);
+                }
+
+                avatar_count += avatars.len();
+                avatars.iter().for_each(|avatar| {
+                    println!("Fetched: {}", avatar.name);
+                    out.push(Avatar {
+                        id: avatar.id.clone(),
+                        name: avatar.name.clone(),
+                        image_url: avatar.image_url.clone(),
+                    });
+                });
+
+                sleep(Duration::from_millis(1000)); // To avoid rate limiting
+            }
+            Err(e) => {
+                return Err(e.to_string());
+            }
+        }
+    }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![fetch_avatars])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Button, MantineProvider } from "@mantine/core";
+import { Button, Input, MantineProvider } from "@mantine/core";
 import '@mantine/core/styles.css';
 import { invoke } from "@tauri-apps/api/core";
 
@@ -7,12 +7,26 @@ function App() {
   //   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
   //   setGreetMsg(await invoke("greet", { name }));
   // }
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const auth_cookie = formData.get("auth_cookie") as string;
+    const twofa_cookie = formData.get("2fa_cookie") as string;
+    console.log("Auth Cookie:", auth_cookie);
+    console.log("2FA Cookie:", twofa_cookie);
+
+    invoke("fetch_avatars", { rawAuthCookie: auth_cookie, raw2faCookie: twofa_cookie })
+  }
 
   return (
     <main>
       <MantineProvider>
         <h1>Hello World!</h1>
-        <Button>Click me!</Button>
+        <form onSubmit={handleSubmit}>
+          <Input name="auth_cookie" placeholder="Auth Cookie" />
+          <Input name="2fa_cookie" placeholder="2FA Cookie" />
+          <Button type="submit">Fetch</Button>
+        </form>
       </MantineProvider>
     </main>
   );

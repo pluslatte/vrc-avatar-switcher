f3ca95bc11c7a1a023358e5fbc2db3a02583adbd
pluslatte <debrisfield0201@gmail.com>
2025-09-13 19:33:45 +0900

    ダークテーマで。まぶしいので。

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <MantineProvider>
+      <MantineProvider forceColorScheme="dark">
         <App />
       </MantineProvider>
     </QueryClientProvider>

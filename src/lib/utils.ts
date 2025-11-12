cc7a50085ba33d10ff84c12187a8305fc07e3aa7
pluslatte <debrisfield0201@gmail.com>
2025-11-12 23:31:45 +0900

    名称でのアバター絞り込み関数を定義

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,20 +1,30 @@
 import { Avatar } from './models';
 
-export const avatarTagSearchfilterAvatars = (
+export const avatarTagSearchFilterAvatars = (
   avatars: Array<Avatar>,
   selectedTags: Array<string>,
   tagAvatarRelation: Record<string, Array<{ display_name: string; color: string }>> | undefined
-) => {
-  const result = avatars.filter(avatar => {
-      if (selectedTags.length === 0) {
-        return true;
-      }
-      if (tagAvatarRelation === undefined) {
-        return false;
-      }
-      const tags = tagAvatarRelation[avatar.id] || [];
-      return selectedTags.every(tag => tags.some(t => t.display_name === tag));
+): Array<Avatar> => (
+  avatars.filter(avatar => {
+    if (selectedTags.length === 0) {
+      return true;
     }
-  );
-  return result;
-};
+    if (tagAvatarRelation === undefined) {
+      return false;
+    }
+    const tags = tagAvatarRelation[avatar.id] || [];
+    return selectedTags.every(tag => tags.some(t => t.display_name === tag));
+  })
+);
+
+export const avatarNameSearchFilterAvatars = (
+  avatars: Array<Avatar>,
+  searchQuery: string,
+): Array<Avatar> => (
+  avatars.filter(avatar => {
+    if (searchQuery.trim() === '') {
+      return true;
+    }
+    return avatar.name.toLowerCase().includes(searchQuery.toLowerCase());
+  })
+);

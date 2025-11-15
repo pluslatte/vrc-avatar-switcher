4ff28740e84df618b34d886eeb39a445ff227d2f
pluslatte <debrisfield0201@gmail.com>
2025-11-15 21:42:05 +0900

    add headers

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C) 2025 pluslatte
+ * This file is part of vrc-avatar-switcher.
+ *
+ * vrc-avatar-switcher is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * 
+ * vrc-avatar-switcher is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with vrc-avatar-switcher. If not, see <https://www.gnu.org/licenses/>. 
+*/
+
 import { Button, Divider, PasswordInput, Stack, Text, TextInput } from '@mantine/core';
 import { useState } from 'react';
 import { command_new_auth, command_2fa, command_email_2fa } from '@/lib/commands';
--- src/components/avatar/TagManagerButton.tsx
+++ src/components/avatar/TagManagerButton.tsx
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C) 2025 pluslatte
+ * This file is part of vrc-avatar-switcher.
+ *
+ * vrc-avatar-switcher is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * 
+ * vrc-avatar-switcher is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with vrc-avatar-switcher. If not, see <https://www.gnu.org/licenses/>. 
+*/
+
 import { Popover, Tooltip, ActionIcon } from '@mantine/core';
 import { IconPlus } from '@tabler/icons-react';
 import TagManager from './TagManager';
